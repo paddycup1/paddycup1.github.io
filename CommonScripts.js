2afea7175fddaea8973a8512632f39d5e8239ff7
PaddyDeng <PaddyDeng@ami.com.tw>
2017-11-03 12:27:55 +0800

    Test exp function.

--- CommonScripts.js
+++ CommonScripts.js
@@ -1,8 +1,28 @@
 window.addEventListener("resize", ResizeContainer);
 
 function ResizeContainer(){
-    console.log(document.getElementById("Container").style.height.toString());
-    console.log(window.innerHeight);
     document.getElementById("Container").style.height = window.innerHeight + "px";
     document.getElementById("Container").style.width = window.innerWidth + "px";
+}
+
+function InitBossList(){
+    var Lists=document.getElementsByClassName('WingBossList');
+    for (i=0; i < Lists.length; i++){
+        Lists[i].originalHeight = Lists[i].clientHeight;
+        Lists[i].style.height = "0px";
+    }
+
+    Lists=document.getElementsByClassName('WingHeader');
+    for (i=0; i < Lists.length; i++){
+        Lists[i].addEventListener("click", SwitchBossList);
+    }
+}
+
+function SwitchBossList(event){
+    var elemnt = event.currentTarget.nextElementSibling;
+    if(elemnt.style.height == "0px"){
+        elemnt.style.height = elemnt.originalHeight + "px";
+    }else{
+        elemnt.style.height = "0px";
+    }
 }
